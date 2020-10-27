29408399e36bfa6428bfa5408f86ba4be2f35239
NathanL <2889205153@qq.com>
2020-10-27 20:03:51 +0800

    Update __init__.py

--- studio/__init__.py
+++ studio/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask,Request
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap#for flask-file-uploader | fileservice
 from .test import tests
 from .fileservice.app import app as fileserviceapp
 from .utils.dir_helper import join_upload_dir
@@ -12,7 +12,6 @@ def create_app():
     app.register_blueprint(fileserviceapp)
     app.config['SECRET_KEY'] = 'hard to guess string'
     app.config['UPLOAD_FOLDER'] = join_upload_dir('data/')
-    #os.path.join(os.getcwd(),'studio/fileservice/data/')
-    app.config['THUMBNAIL_FOLDER'] = join_upload_dir('data/thumbnail/')#'data/thumbnail/'
+    app.config['THUMBNAIL_FOLDER'] = join_upload_dir('data/thumbnail/')
     app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024
     return app
